ead56e466f6fe3975797dde95c97a2feed569f91
Michal Muskala <michal@muskala.eu>
2016-06-27 21:02:53 +0200

    Optimize line rendering

--- LePaint/Objects/Line.cs
+++ LePaint/Objects/Line.cs
@@ -8,21 +8,19 @@ namespace LePaint.Objects
 {
     internal class Line : IObject
     {
-        private readonly IList<Point> points;
         private readonly Pen pen;
-        private readonly PathPointType option;
+        private readonly GraphicsPath path;
  
         public Line(Pen pen, IList<Point> points, string option)
         {
-            this.points = points;
             this.pen = pen;
-            this.option = PathPointType.Line; // TODO: convert from option
+            var convertedOption = PathPointType.Line; // TODO: convert from option
+            var pointTypes = points.Select(_ => (byte)convertedOption).ToArray();
+            this.path = new GraphicsPath(points.ToArray(), pointTypes);
         }
 
         public void Draw(Graphics graphics)
         {
-            var pointTypes = points.Select(_ => (byte) option).ToArray();
-            var path = new GraphicsPath(points.ToArray(), pointTypes);
             graphics.DrawPath(pen, path);
         }
     }
