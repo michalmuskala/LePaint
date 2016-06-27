09b726ebf7cb1157522bfc8576146b6edcd41129
Michal Muskala <michal@muskala.eu>
2016-06-27 19:00:03 +0200

    Do not use new syntax

--- LePaint/LePaintView.cs
+++ LePaint/LePaintView.cs
@@ -30,18 +30,45 @@ namespace LePaint
             set { /* show fide the checkbox */ }
         }
 
+        public IEnumerable<string> BrushOptions
+        {
+            set
+            {
+                /* a combobox with those options */
+            }
+        }
+
         public event EventHandler<IEnumerable<Point>> PathUpdated;
-        public event EventHandler<string> BrushSelected;
+        public event EventHandler<string> BrushSelected; // Call when brush selected
         public event EventHandler<Color> SelectedColor;
         public event EventHandler<int> SelectedSize;
+        public event EventHandler<string> OptionSelected; // Call on option selection
         public event EventHandler Commit;
 
         public LePaintView()
         {
             InitializeComponent();
             DoubleBuffered = true;
-            plotno1.PathUpdated = path => PathUpdated?.Invoke(this, path);
-            plotno1.Commit = () => Commit?.Invoke(this, new EventArgs());
+            plotno1.PathUpdated = OnPathUpdated;
+            plotno1.Commit = OnCommit;
+        }
+
+        private void OnCommit()
+        {
+            var handlers = Commit;
+            if (handlers != null)
+            {
+                handlers(this, new EventArgs());
+            }
+        }
+
+        private void OnPathUpdated(IEnumerable<Point> obj)
+        {
+            var handlers = PathUpdated;
+            if (handlers != null)
+            {
+                handlers(this, obj);
+            }
         }
 
         private void colorPicker_Click(object sender, EventArgs e)
@@ -50,8 +77,17 @@ namespace LePaint
             dialog.Color = colorPicker.BackColor;
             dialog.ShowDialog();
             colorPicker.BackColor = dialog.Color;
-            SelectedColor?.Invoke(this, dialog.Color);
-            Commit?.Invoke(this, new EventArgs());
+            OnSelectedColor(dialog.Color);
+            OnCommit();
+        }
+
+        private void OnSelectedColor(Color color)
+        {
+            var handlers = SelectedColor;
+            if (handlers != null)
+            {
+                handlers(this, color);
+            }
         }
 
         private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
